d8b2ad5f3c71bf8c494eeea61b1235895cd52760
Samnjor-alpha <njoroges740@gmail.com>
2022-03-10 20:32:55 +0300

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+akili mtu wangu
  */
 int main(void)
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * /**
+   akili mtu wangu
+
  * Return: Always 0 (Success)
  */
 int main(void)
