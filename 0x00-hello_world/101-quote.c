33f17d31e6642fc47b1f87f9946c38ccefe6432a
Samnjor-alpha <njoroges740@gmail.com>
2022-03-10 20:37:17 +0300

    * Return: Always 0 (Success)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,10 @@
 #include <unistd.h>
 
 /**
-*no comment.Be wise
-* Return: Always 0 (Success)
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
