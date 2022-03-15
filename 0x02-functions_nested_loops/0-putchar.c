daaeb20116ba128ddf5c7d96436369129658bf14
Samnjor <njoroges740@gmail.com>
2022-03-15 21:40:27 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,12 @@
 
 /**
  * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
+ * Description: It prints the word _putchar, followed by a new line.
  * Return: 0.
  */
 int main(void)
 {
-	char word[9] = "Holberton";
+	char word[9] = "_putchar";
 	int i;
 
 	for (i = 0; i < 9; i++)
