5ff29a082c3851fc9f4ab0f9f0492217cbcc209b
Samnjor <njoroges740@gmail.com>
2022-03-15 21:43:44 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	char word[10] = "_putchar";
+	char word[8] = "_putchar";
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
 	_putchar('\n');
 
