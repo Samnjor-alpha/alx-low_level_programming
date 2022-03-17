c1cdbee042cd2e23621a4fbfd4a00e97a1df3fee
Samnjor <njoroges740@gmail.com>
2022-03-17 21:21:19 +0300

    Trophies

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,29 +8,16 @@
   */
  void print_number(int n)
  {
- 	if (n == 0)
- 		_putchar('0');
- 	else if (n < 0)
+ 	unsigned int num = n;
+
+ 	if (n < 0)
  	{
  		_putchar('-');
- 		print_integer(n * -1);
+ 		num = -num;
  	}
- 	else
- 		print_integer(n);
- }
 
- /**
-  * print_integer - A function to priting n
-  * @m: an input unsigned integer
-  * Return: Nothing
-  */
- void print_integer(int m)
- {
- 	int i = 1000000000;
+ 	if ((num / 10) > 0)
+ 		print_number(num / 10);
 
- 	for (; i >= 1; i /= 10)
- 		if (m / i != 0)
- 		{
- 			_putchar((m / i) % 10 + '0');
- 		}
+ 	_putchar((num % 10) + '0');
  }
