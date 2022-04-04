b93fa5e4a288ca138a30a0bcf5bfc93bfd879713
BoredApe <njoroges740@gmail.com>
2022-04-04 20:00:50 +0300

    I will show you how great I am

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -10,12 +10,12 @@
  */
 void print_tab(char **tab)
 {
-    int i;
+	int i;
 
-    for (i = 0; tab[i] != NULL; ++i)
-    {
-        printf("%s\n", tab[i]);
-    }
+	for (i = 0; tab[i] != NULL; ++i)
+	{
+		printf("%s\n", tab[i]);
+	}
 }
 
 /**
@@ -25,14 +25,14 @@ void print_tab(char **tab)
  */
 int main(void)
 {
-    char **tab;
+	char **tab;
 
-    tab = strtow("Talk is cheap. Show me the code");
-    if (tab == NULL)
-    {
-        printf("Failed\n");
-        return (1);
-    }
-    print_tab(tab);
-    return (0);
+	tab = strtow("      Holberton School         #cisfun      ");
+	if (tab == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	print_tab(tab);
+	return (0);
 }
